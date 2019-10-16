0652bb681d3b8bc555d6eea26d54f24687d1acfc
Rebecca Stevens <rsbjshan@gmail.com>
2019-10-16 10:38:53 +0100

    PHP basic syntax

---
+++ testFolder/testPHP.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p style="color: hotpink; font-size: 25px;">This is an html in a php file</p>
+</body>
+</html>
+
+<script>
+alert("This is a Javascript inside the PHP file");
+</script>
+<?php echo"This is a php tag"?>
+
+<?php
+
+//Everything here becomes PHP for a comment
+
+/*Another comment
+another comment 
+multiple line comment*/
+
+echo "This is a PHP text";
+
+echo 45;
+
+echo "<p>This is an HTML paragraph</p>";
+
+?>
