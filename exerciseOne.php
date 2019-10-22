61e59f48668db90acf837473241b18a5b8b5b1d1
Rebecca Stevens <rsbjshan@gmail.com>
2019-10-22 10:53:07 +0100

    Carrying on from the basic php foundation . Exercises , assigning variables, associative arrays and using the elsif, else statements.

---
+++ exerciseOne.php
@@ -0,0 +1,39 @@
+<?php
+
+// Making Variables
+
+$performer = "Kelvin Hart";
+$country = "dubai";
+$performanceDate = "4th April 2016";
+
+$performerAge = 30;
+$ageMin = 18;
+$ageMax = 49;
+
+//Associative Array
+
+$AssociativeArrayPerformer = array("Fullname" => "Kelvin Hart", "Height" => "5 Inches", "Age" => "30");
+
+echo $AssociativeArrayPerformer[Height];
+
+if($performerAge <= $ageMin){
+    echo "Performer is too young";
+}elseif($performerAge >= $ageMax){
+    echo "Performer is too old";
+}else{
+    echo "Performer is apropriate age";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
